fb16e43812da67ff96ae2cc9b07017d8881be8c2
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2020-03-31 17:37:11 +0000

    Poner serie en descripcion de linea de factura y poder imprimir cuenta aunque no tenga firma fel

--- models/pos_order.py
+++ models/pos_order.py
@@ -8,3 +8,10 @@ class PosOrder(models.Model):
     firma_fel = fields.Char('Firma FEL', related='account_move.firma_fel')
     serie_fel = fields.Char('Serie FEL', related='account_move.serie_fel')
     numero_fel = fields.Char('Numero FEL', related='account_move.numero_fel')
+
+    def _prepare_invoice_line(self, order_line):
+        res = super(PosOrder, self)._prepare_invoice_line(order_line)
+        if order_line.pack_lot_ids:
+            lotes = ', '.join([l.lot_name for l in order_line.pack_lot_ids])
+            res['name'] += ': '+lotes
+        return res
