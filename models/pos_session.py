f70363b9c7bb49a3db01c7bbe57e1268d5408e86
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2020-12-02 16:07:47 +0000

    No validar pedidos con valor 0 al hacer cierre

--- models/pos_session.py
+++ models/pos_session.py
@@ -9,11 +9,10 @@ class PosSession(models.Model):
     def action_pos_session_close(self):
         for session in self:
             if session.config_id.invoice_journal_id and session.config_id.invoice_journal_id.generar_fel:
-                orders = session.order_ids.filtered(lambda order: order.state != 'invoiced' and order.amount_total > 0)
-                if len(orders) > 0:
+                if len(session.order_ids.filtered(lambda order: order.state != 'invoiced' and order.amount_total > 0)) > 0:
                     raise ValidationError('Tiene pedidos sin factura, no puede cerrar sesión mientras no haya facturado todos los pedidos.')
-                for order in session.order_ids.filtered(lambda order: order.state == 'invoiced'):
+                for order in session.order_ids.filtered(lambda order: order.state == 'invoiced' and order.amount_total > 0):
                     if order.account_move.state != 'open' and not order.account_move.firma_fel:
-                        raise ValidationError('La factura del pedido {} no está firmada, por ingrese a la factura valídela para poder cerrar sesión.'.format(order.name))
+                        raise ValidationError('La factura del pedido {} no está firmada, por favor ingrese a la factura y validela para poder cerrar sesión.'.format(order.name))
 
         res = super(PosSession, self).action_pos_session_close()
