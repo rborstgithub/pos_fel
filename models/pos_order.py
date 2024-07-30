45c46e3016a3282fd0cfbadbc268d704861aa789
rocketgithub <rodrigo@aquih.com>
2024-07-30 14:12:36 -0600

    Faltaba cerrar comilla

--- models/pos_order.py
+++ models/pos_order.py
@@ -22,7 +22,7 @@ class PosOrder(models.Model):
         res = super(PosOrder, self)._prepare_invoice_vals()
         if self.refunded_orders_count == 1 and self.refunded_order_ids.account_move:
             res['factura_original_id'] = self.refunded_order_ids.account_move.id
-            res['motivo_fel'] = 'Anulación
+            res['motivo_fel'] = 'Anulación'
         if self.refunded_order_ids and self.refunded_order_ids[0].account_move:
             res['factura_original_id'] = self.refunded_order_ids[0].account_move.id
             res['motivo_fel'] = 'Anulación'
