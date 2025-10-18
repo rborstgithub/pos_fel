fd8aa8e03dd46b1e1e33e753d02a74550840960f
rocketgithub <rodrigo@aquih.com>
2025-10-17 18:39:59 -0600

    Llamada innecesaria

--- static/src/overrides/components/partner_list/partner_list.js
+++ static/src/overrides/components/partner_list/partner_list.js
@@ -8,7 +8,7 @@ patch(PartnerList.prototype, {
         let result = await super.getNewPartners();
         if (!result.length) {
             result = await this.pos.data.silentCall("pos.session", "crear_partner_con_datos_sat", [this.pos.company.id, this.state.query]);
-            let newPartners = await this.getNewPartners();
+            //let newPartners = await this.getNewPartners();
             //if (newPartners.length) {
             //    this.clickPartner(newPartners[0]);
             //}
