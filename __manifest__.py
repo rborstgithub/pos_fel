86b5ffb7de6867564b8f4953d64886465496af57
Rodrigo Fernández <rodrigo@aquih.com>
2025-12-03 21:52:00 +0000

    Imprimir etiqueta y para llevar

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Point of Sale unido a facturacion electrónica',
-    'version': '1.4',
+    'version': '1.5',
     'category': 'Point of Sale',
     'sequence': 6,
     'summary': 'Point of Sale unido a facturacion electrónica',
